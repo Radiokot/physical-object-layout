5a71ea027772c398c6dc0a5a4828811f023ca285
Oleg Koretsky <test.radiokot@gmail.com>
2020-10-03 15:34:54 +0300

    Add initial project structure with sample app

---
+++ sample-app/src/main/java/ua/com/radiokot/physicalitemlayoutsample/MainActivity.kt
@@ -0,0 +1,11 @@
+package ua.com.radiokot.physicalitemlayoutsample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
